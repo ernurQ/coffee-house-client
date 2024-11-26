2b66bed04b61ab279a409464feed24bc3444e665
ernurQ <garifullin.ernur@gmail.com>
2024-11-26 17:06:13 +0500

    fix(navbar): made navbar absolute

--- src/app/(client)/_ui/Navbar.tsx
+++ src/app/(client)/_ui/Navbar.tsx
@@ -3,7 +3,7 @@ import { Nav } from './Nav'
 export function Navbar() {
 	return (
 		<div
-			className={'fixed w-full md:w-3/6 md:pl-4 lg:w-2/6 pt-3 z-50'}
+			className={'absolute top-0 w-full md:w-3/6 md:pl-4 lg:w-2/6 pt-3 z-50'}
 			style={{ textShadow: '1px 1px 5px black' }}
 		>
 			<Nav variant={'white'} />
